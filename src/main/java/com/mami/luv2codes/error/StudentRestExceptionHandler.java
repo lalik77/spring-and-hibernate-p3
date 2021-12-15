ac1e47b286ec3f4cab8be8818dfdfe7593f37e8d
Alexander Mami <algru81@gmail.com>
2021-12-15 16:08:52 +0300

    rename class and finsidh module 61

--- src/main/java/com/mami/luv2codes/error/StudentRestExceptionHandler.java
+++ src/main/java/com/mami/luv2codes/error/StudentRestExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class StudentRestExceptioHandler {
+public class StudentRestExceptionHandler {
 
   @ExceptionHandler
   public ResponseEntity<StudentResponseError> handleException(StudentNotFoundException e) {
