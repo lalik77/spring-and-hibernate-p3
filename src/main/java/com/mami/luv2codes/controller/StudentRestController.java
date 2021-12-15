ce3f080d28a8886dc79f73c198f2441c544d8fdc
Alexander Mami <algru81@gmail.com>
2021-12-15 16:04:13 +0300

    remove exception handling from controller and add exception handler class

--- src/main/java/com/mami/luv2codes/controller/StudentRestController.java
+++ src/main/java/com/mami/luv2codes/controller/StudentRestController.java
@@ -50,30 +50,4 @@ public class StudentRestController {
     return theStudents.get(id);
   }
 
-  @ExceptionHandler
-  public ResponseEntity<StudentResponseError> handleException(StudentNotFoundException e) {
-
-    // Create new Student response error
-
-    StudentResponseError studentResponseError = new StudentResponseError();
-    studentResponseError.setStatus(HttpStatus.NOT_FOUND.value());
-    studentResponseError.setMessage(e.getMessage());
-    studentResponseError.setTimeStamp(System.currentTimeMillis());
-
-    //Return response entity
-    return new ResponseEntity<>(studentResponseError,HttpStatus.NOT_FOUND);
-
-
-  }
-  @ExceptionHandler
-  public ResponseEntity<StudentResponseError> handleException(Exception e) {
-
-    StudentResponseError studentResponseError = new StudentResponseError();
-    studentResponseError.setStatus(HttpStatus.BAD_REQUEST.value());
-    studentResponseError.setMessage(e.getMessage());
-    studentResponseError.setTimeStamp(System.currentTimeMillis());
-
-    return new ResponseEntity<>(studentResponseError,HttpStatus.BAD_REQUEST);
-
-  }
 }
