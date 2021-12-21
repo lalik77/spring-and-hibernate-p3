9327365971d6b685bb968265151bd36aced0e763
Alexander Mami <algru81@gmail.com>
2021-12-21 17:12:32 +0300

    Finish lesson 66

--- src/main/java/com/mami/luv2codes/controller/CustomerRestController.java
+++ src/main/java/com/mami/luv2codes/controller/CustomerRestController.java
@@ -6,6 +6,8 @@ import com.mami.luv2codes.service.CustomerService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +44,18 @@ public class CustomerRestController {
     }
 
     return theCustomer;
+  }
+
+  @PostMapping("/customers")
+  public Customer add(@RequestBody Customer theCustomer) {
+
+    //also just in case the pass in id in Json .. set id to 0
+    //this is force a save of new item - instead of update because Hibernate
+
+    theCustomer.setId(0);
+
+    customerService.saveCustomer(theCustomer);
+    return theCustomer;
+
   }
 }
